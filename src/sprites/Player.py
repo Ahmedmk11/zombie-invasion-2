ed6b10d8b41282cc08114be6bc6b6b1eeb5f1ac4
Ahmed <ahmedmahmoudkzm@gmail.com>
2022-07-09 01:33:53 +0200

    Added Bullet, Fireball and Platform classes & Finished Player constructor

--- src/sprites/Player.py
+++ src/sprites/Player.py
@@ -1,28 +1,54 @@
-import pygame as pg
+import pygame as pg, os
 from pygame import Vector2 as vec
 pg.init()
 
 WIDTH = 1306
 HEIGHT = 526 
 
+def makeAnimation(directory):
+    list1 = os.listdir(directory)
+    n = len(list1)
+    tempList = []
+    for i in range(1,n+1):
+        if (os.path.exists(f'{directory}{i}.png')):
+            tmp = pg.image.load(f'{directory}{i}.png')
+            tmp.set_colorkey(0,0,0)
+            tempList.append(tmp)
+    
+    return tempList
+
+def makeAnimationFlip(directory):
+    list1 = os.listdir(directory)
+    n = len(list1)
+    tempList = []
+    for i in range(1,n+1):
+        if (os.path.exists(f'{directory}{i}.png')):
+            tmp = pg.image.load(f'{directory}{i}.png')
+            tmp.set_colorkey(0,0,0)
+            tempList.append(pg.transform.flip(tmp,True,False))
+    
+    return tempList
+
 class Player(pg.sprite.Sprite):
     def __init__(self, filename):
         super().__init__()
         self.filename = filename
-        self.spriteSheet = pg.image.load(filename).convert()
         self.hp = 100
-        self.isShoot = False
+        self.isShooting = False
         self.isDead = False
-        self.isJump = False
+        self.isJumping = False
         self.isIdle = True
-        self.coolDown = 0
+        self.isFlipped = False
+        self.shootingCoolDown = 0
         self.pos = vec(WIDTH/2,HEIGHT/2)
-
-    def getSprite(self, x, y, w, h):
-        sprite = pg.Surface((w,h))
-        sprite.set_colorkey((0,0,0))
-        sprite.blit(self.spriteSheet,(0,0),(x,y,w,h))
-        return sprite
+        self.walkLeft = makeAnimation('resources/images/sprites/heroine/walk/')
+        self.walkRight = makeAnimationFlip('resources/images/sprites/heroine/walk/')
+        self.idleLeft = makeAnimation('resources/images/sprites/heroine/idle/')
+        self.idleRight = makeAnimationFlip('resources/images/sprites/heroine/idle/')
+        self.shootLeft = makeAnimation('resources/images/sprites/heroine/shoot/')
+        self.shootRight = makeAnimationFlip('resources/images/sprites/heroine/shoot/')
+        self.dieLeft = makeAnimation('resources/images/sprites/heroine/die/')
+        self.dieRight = makeAnimationFlip('resources/images/sprites/heroine/die/')
 
     def move():
         pass
