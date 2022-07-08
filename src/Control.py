739bc375d4a7268b7881fd6fd2d237f28238658a
Ahmed <ahmedmahmoudkzm@gmail.com>
2022-07-08 21:35:11 +0200

    Cropped all heroine sprites

--- src/Control.py
+++ src/Control.py
@@ -1,4 +1,3 @@
 import pygame as pg,sys
 from pygame import mixer
 pg.init()
-
--- src/sprites/Player.py
+++ src/sprites/Player.py
@@ -1,11 +1,29 @@
 import pygame as pg
+from pygame import Vector2 as vec
 pg.init()
 
+WIDTH = 1306
+HEIGHT = 526 
+
 class Player(pg.sprite.Sprite):
-    def __init__(self):
+    def __init__(self, filename):
         super().__init__()
+        self.filename = filename
+        self.spriteSheet = pg.image.load(filename).convert()
+        self.hp = 100
+        self.isShoot = False
+        self.isDead = False
+        self.isJump = False
+        self.isIdle = True
         self.coolDown = 0
-        
+        self.pos = vec(WIDTH/2,HEIGHT/2)
+
+    def getSprite(self, x, y, w, h):
+        sprite = pg.Surface((w,h))
+        sprite.set_colorkey((0,0,0))
+        sprite.blit(self.spriteSheet,(0,0),(x,y,w,h))
+        return sprite
+
     def move():
         pass
 
